6c18c3faf9330c972f5c568b1ce25a677d1de001
blair drummond <blair.drummond@statcan.gc.ca>
2022-01-14 09:59:57 -0500

    feat(gitea): add initial gitea implementation

---
+++ terraform/gitea.tf
@@ -0,0 +1,25 @@
+resource "kubernetes_namespace" "gitea" {
+  metadata {
+    name = "gitea"
+  }
+}
+
+
+resource "random_string" "gitea_admin" {
+  length           = 32
+  special          = true
+}
+
+# READ Token
+resource "kubernetes_secret" "gitea_admin_secret" {
+  metadata {
+    name = "gitea-admin-secret"
+    namespace = "gitea"
+  }
+
+  data = {
+    "username" = "admin"
+    "password" = random_string.gitea_admin.result
+    "email" = "blairdrummond@protonmail.com"
+  }
+}
